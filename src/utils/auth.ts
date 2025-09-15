30240229869bd62874ad1535b50d13aa36e7724f
Antoine Lelong <azenoxe@gmail.com>
2025-09-15 17:20:38 +0200

    fix: remove new prisma client in auth file and use db singleton instead

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,14 +1,12 @@
 import { betterAuth } from "better-auth";
 import { nextCookies } from "better-auth/next-js";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@prisma/client";
 import { genericOAuth } from "better-auth/plugins";
 import { jwtDecode } from "jwt-decode";
-
-const prisma = new PrismaClient();
+import { db } from "~/server/db";
 
 export const auth = betterAuth({
-	database: prismaAdapter(prisma, {
+	database: prismaAdapter(db, {
 		provider: "postgresql",
 	}),
 	user: {
